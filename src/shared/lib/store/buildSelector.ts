0d6993261d5e5a2da19b437c64e0503dbd8bfa01
DzmitryKas <kasperovich_dima@mail.ru>
2024-01-29 22:18:05 +0300

    add helpers for slice

---
+++ src/shared/lib/store/buildSelector.ts
@@ -0,0 +1,13 @@
+import { type IStateSchema } from '@/app/providers/StoreProvider'
+import { useSelector } from 'react-redux'
+
+type TSelector<T> = (state: IStateSchema) => T
+type TResult<T> = [() => T, TSelector<T>]
+
+export function buildSelector<T> (selector: TSelector<T>): TResult<T> {
+    const useSelectorHook = () => {
+        return useSelector(selector)
+    }
+
+    return [useSelectorHook, selector]
+}
